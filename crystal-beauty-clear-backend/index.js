9d505bf1da860da9f2140275ddc0bb42cd517593
ACWeerathunga <aweerathunga20@gmail.com>
2025-03-18 22:12:20 +0530

    initializing the project ✏️

---
+++ crystal-beauty-clear-backend/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import userRouter from './routes/userRouter.js';
+import jwt from "jsonwebtoken";
+import productRouter from './routes/productRouter.js';
+import orderRouter from './routes/orderRouter.js';
+const app = express();
+
+//mongodb+srv://Amantha:<db_password>@cluster0.ec3y1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongoose.connect("mongodb+srv://Amantha:123@cluster0.ec3y1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+    ()=>{
+        console.log("Connected to the database");
+    }
+).catch(
+    ()=>{
+        console.log("Connection failed");
+    }
+)
+
+
+
+
+app.use(bodyParser.json());
+app.use(
+    (req,res,next)=>{
+        const header = req.header("Authorization");
+        if(header != null){
+            const token = header.replace("Bearer ","")
+            jwt.verify(token,"random456",(err , decoded)=>{
+                console.log(decoded)
+                if(decoded != null){
+                    req.user = decoded
+                }
+            })
+        }
+        next()
+    }
+)
+
+
+
+app.use("/api/user" , userRouter)
+app.use("/api/product", productRouter)
+app.use("/api/order",orderRouter)
+
+
+
+app.listen(5000, 
+    ()=>{
+        console.log("Server is running on port 5000");
+    }
+)
